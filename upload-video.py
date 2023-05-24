2e4d1b43ad0da7e20465c2a5597528a05f3f5e67
Александр Парфенов <a.parfenov@iac.spb.ru>
2023-05-24 15:30:48 +0300

    еще

--- upload-video.py
+++ upload-video.py
@@ -1,4 +1,4 @@
-#!/home/parf/upload-video/venv/bin/python
+#!/home/aparfenov/projects/upload-video/venv/bin/python
 
 import http.client
 import httplib2
@@ -134,14 +134,12 @@ def replace_placeholders(filename, str, video_name=None):
   with open(filename, 'r') as file:
       lines = file.readlines()
 
-  placeholders_count = str.count(PLACEHOLDER)
-  if placeholders_count > len(lines):
-      placeholders_count = len(lines)
-  random_strings = random.sample(lines, placeholders_count)
+  replacement_count = min(str.count(PLACEHOLDER), len(lines))
+  random_strings = random.sample(lines, replacement_count)
   output_string = str
 
   for random_string in random_strings:
-    replaced_string = output_string.replace("#" + PLACEHOLDER, random_string.strip().replace(" ", "_"), 1)
+    replaced_string = output_string.replace("#" + PLACEHOLDER, "#" + random_string.strip().replace(" ", ""), 1)
     if replaced_string == output_string:
       output_string = output_string.replace(PLACEHOLDER, random_string.strip(), 1)
     else:
